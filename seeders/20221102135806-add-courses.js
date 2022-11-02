96225bb13a9ff2cd036563de1248413ca72534ec
Your Name <you@example.com>
2022-11-02 09:24:46 -0500

    Controllers y seed

--- seeders/20221102135806-add-courses.js
+++ seeders/20221102135806-add-courses.js
@@ -9,28 +9,32 @@ module.exports = {
        description: 'Curso avanzado de JS - NodeJs',
        weeks: '10',
        enroll_cost:'1000',
-       minimum_skill:'JS Basico'
+       minimum_skill:'JS Basico',
+       user_id: 1
      },
      {
       title: 'Java - SpringBoot',
       description: 'Inducción a SpringBoot',
       weeks: '2',
       enroll_cost:'500',
-      minimum_skill:'Java avanzado'
+      minimum_skill:'Java avanzado',
+      user_id: 2
     },
     {
       title: 'SQL - MySQL',
       description: 'Inducción a MYSQL',
       weeks: '4',
       enroll_cost:'5000',
-      minimum_skill:'Conocimientos básicos de UML'
+      minimum_skill:'Conocimientos básicos de UML',
+      user_id: 3
     },
     {
       title: 'MongoBD',
       description: 'Inducción a MongoDB',
       weeks: '6',
       enroll_cost:'5000',
-      minimum_skill:'Conocimientos básicos de JSON'
+      minimum_skill:'Conocimientos básicos de JSON',
+      user_id: 4
     }
     
    ], {});
